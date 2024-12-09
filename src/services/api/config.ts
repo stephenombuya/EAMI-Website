8d6adcdd86af5299b95ce9d53ac6bf0d19d82a7f
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2024-12-09 14:11:54 +0300

    Create config.ts

---
+++ src/services/api/config.ts
@@ -0,0 +1,8 @@
+export const API_CONFIG = {
+  MAPBOX_TOKEN: 'your_mapbox_token',
+  USGS_API_URL: 'https://mrdata.usgs.gov/api/v1',
+  NOMINATIM_API_URL: 'https://nominatim.openstreetmap.org',
+  WORLD_BANK_API_URL: 'https://api.worldbank.org/v2',
+  UNEP_API_URL: 'https://api.unep.org/v1',
+  ITC_API_URL: 'https://api.intracen.org/v1',
+};
