105a96c5bd72299d540eb802324eca83860be5f6
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2025-01-29 21:27:25 +0300

    Create error.js

---
+++ backend/src/utils/error.js
@@ -0,0 +1,8 @@
+export class ApiError extends Error {
+  constructor(message, statusCode = 500, code = 'INTERNAL_ERROR') {
+    super(message);
+    this.statusCode = statusCode;
+    this.code = code;
+    this.name = 'ApiError';
+  }
+}
